db1c31f589f5d32c98e40890458b337c4652d01c
ror <ror@ror-laptop.(none)>
2012-03-28 19:56:10 +0600

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UProfit::Application.config.secret_token = 'c275367dfdea0dc3950ce1f56538e0ccddc18382da321c9cc0d2246be595718ef8cca1c5e5ce2e94c9578bd7d78b83ecf6f106031060a95b9ec3f1f422c3c078'
